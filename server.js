00e2eefa9cd3bdf30467ad403b226fbbac7dadc3
Rodrigo Cardoso Da Conceicao <rodrigo.cardoso832@gmail.com>
2023-12-06 00:10:12 -0300

    Correcao do caminho da route

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const express = require ('express')
 const cors = require('cors');
 const path = require ('path')
 const app = express ()
-const apiV1Router = require('./routes/apiV1Router');
-const apiV2Router = require('./routes/apiV2Router');
+const apiV1Router = require('./api/routes/apiV1Router');
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/app', express.static (path.join (__dirname, '/public')))
 app.use('/api/v1', apiV1Router);//produtos
-app.use('/api/v2', apiV2Router);//usuarios
 
 
 let port = process.env.PORT || 3000
