4531191b3dc44e6384b93b7dc09ef1aab7d15db8
Rodrigo Cardoso Da Conceicao <rodrigo.cardoso832@gmail.com>
2023-12-05 23:18:57 -0300

    Ajustando a configuração do banco de dados no render na minha aplicação

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,13 @@
-// knexfile.js
 module.exports = {
-    client: 'pg',
-    connection: {
-      host: 'dpg-clnsmdeqc21c73dvotl0-a',
-      port: 5432,
-      database: 'nodeapp_postgresql_instance',
-      user: 'nodeapp_postgresql_instance_user',
-      ssl: { rejectUnauthorized: false },
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations',
-    },
-  };
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './migrations',
+  },
+};
+
   
